630c19d3c056e3d129480719df881cbf0e669420
azerbelhedi <azerbelhedide@gmail.com>
2019-04-05 01:07:18 +0100

    Updates

---
+++ precache-manifest.dbb2a3c9e8001cd34f559fa954a1d6cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56c708c9ded9eb3c4322",
+    "url": "/azerbelhedi/fastfFood.git/static/js/runtime~main.50261eb6.js"
+  },
+  {
+    "revision": "f5256d3847f300dbd679",
+    "url": "/azerbelhedi/fastfFood.git/static/js/main.82368b95.chunk.js"
+  },
+  {
+    "revision": "9cd7dbe2c5549d53f4ae",
+    "url": "/azerbelhedi/fastfFood.git/static/js/2.ab810f9c.chunk.js"
+  },
+  {
+    "revision": "f5256d3847f300dbd679",
+    "url": "/azerbelhedi/fastfFood.git/static/css/main.e1739abc.chunk.css"
+  },
+  {
+    "revision": "69bedcca9637d80bde9b4bef3dc142cc",
+    "url": "/azerbelhedi/fastfFood.git/index.html"
+  }
+];
